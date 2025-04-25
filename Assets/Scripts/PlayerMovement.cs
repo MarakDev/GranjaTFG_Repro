1b26a4a8fb2a0af97b27911c82f8652611fe89e3
Marak <marak32@outlook.es>
2025-04-25 23:32:42 +0200

    TFG v.0.5 - Añadido sprites perro

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,12 +10,16 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private float speed;
 
-    [SerializeField] private float rabitAreaRange;
-    [SerializeField] private LayerMask rabitsLayer;
+    [SerializeField] private float wolfRange;
+    [SerializeField] private LayerMask wolfLayer;
 
+    //animator
+    private Animator animatorD1;
+    private Animator animatorD2;
+
+    
 
     //var privates
-
     private GameObject dog1;
     private GameObject dog2;
 
@@ -47,6 +51,11 @@ public class PlayerMovement : MonoBehaviour
         dog1_rb = dog1.GetComponent<Rigidbody2D>();
         dog2_rb = dog2.GetComponent<Rigidbody2D>();
 
+        animatorD1 = dog1.GetComponent<Animator>();
+        animatorD2 = dog2.GetComponent<Animator>();
+
+        animatorD1.Play("Idle");
+        animatorD2.Play("Idle");
     }
 
     private void Update()
@@ -56,10 +65,12 @@ public class PlayerMovement : MonoBehaviour
         dog2_moveDirection = dog2_input.ReadValue<Vector2>();
 
         if (dog1 != null)
-            dog1Area = Physics2D.OverlapCircle(dog1.transform.position, rabitAreaRange, rabitsLayer);
+            dog1Area = Physics2D.OverlapCircle(dog1.transform.position, wolfRange, wolfLayer);
         if (dog2 != null)
-            dog2Area = Physics2D.OverlapCircle(dog2.transform.position, rabitAreaRange, rabitsLayer);
+            dog2Area = Physics2D.OverlapCircle(dog2.transform.position, wolfRange, wolfLayer);
 
+        UpdateAnimationD1();
+        UpdateAnimationD2();
     }
 
     private void FixedUpdate()
@@ -68,24 +79,51 @@ public class PlayerMovement : MonoBehaviour
         dog2_rb.velocity = new Vector2(dog2_moveDirection.x * speed, dog2_moveDirection.y * speed);
     }
 
-    private void OnDrawGizmos()
+    private void UpdateAnimationD1()
     {
-        Gizmos.color = Color.yellow;
+        if (dog1_rb.velocity.x > 0.2f)
+            dog1.transform.localScale = new Vector3(2.25f, dog1.transform.localScale.y, dog1.transform.localScale.z);
+        else
+            dog1.transform.localScale = new Vector3(-2.25f, dog1.transform.localScale.y, dog1.transform.localScale.z);
 
-        if (dog1Area)
-            Gizmos.color = Color.red;
-
-        if (dog1 != null)
-            Gizmos.DrawWireSphere(dog1.transform.position, rabitAreaRange);
-
-        Gizmos.color = Color.yellow;
-
-        if(dog2Area)
-            Gizmos.color = Color.red;
-
-        if(dog2 != null)
-            Gizmos.DrawWireSphere(dog2.transform.position, rabitAreaRange);
+        if (dog1_rb.velocity.magnitude < 0.2f)
+        {
+            if (dog1Area) //si esta en rango de lobo
+                animatorD1.Play("Idle_Shooting");
+            else
+                animatorD1.Play("Idle");
+        }else if(dog1_rb.velocity.magnitude > 0.2f)
+        {
+            if (dog1Area) //si esta en rango de lobo
+                animatorD1.Play("Run_Shooting");
+            else
+                animatorD1.Play("Run");
+        }
     }
+
+    private void UpdateAnimationD2()
+    {
+        if (dog2_rb.velocity.x > 0.2f)
+            dog2.transform.localScale = new Vector3(2.25f, dog2.transform.localScale.y, dog2.transform.localScale.z);
+        else
+            dog2.transform.localScale = new Vector3(-2.25f, dog2.transform.localScale.y, dog2.transform.localScale.z);
+
+        if (dog2_rb.velocity.magnitude < 0.2f)
+        {
+            if (dog2Area) //si esta en rango de lobo
+                animatorD2.Play("Idle_Shooting");
+            else
+                animatorD2.Play("Idle");
+        }
+        else if (dog2_rb.velocity.magnitude > 0.2f)
+        {
+            if (dog2Area) //si esta en rango de lobo
+                animatorD2.Play("Run_Shooting");
+            else
+                animatorD2.Play("Run");
+        }
+    }
+
 
     private void OnEnable()
     {
